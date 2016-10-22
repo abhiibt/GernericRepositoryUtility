2f4f7840015a844fc71d491db3c6e4df58104b87
abhiibt <abhiibt85@gmail.com>
2016-10-22 22:09:29 +0200

    Generic Repository implementation with Test projects

---
+++ GenericRepositoryUntility/GenericRepositoryUntilityTests/RepositoryTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using Example.DataAccessLayer;
+using Example.DataAccessLayer.DbModel;
+using Ninject;
+using NUnit.Framework;
+
+namespace GenericRepositoryUntilityTests
+{
+    [TestFixture]
+    public class RepositoryTests
+    {
+        [Test]
+        public void SubjectRepositoryTest_Add_Subjects()
+        {
+            using (var kernel = new StandardKernel(new ExampleNinjectModule()))
+            {
+                var studentRepository = kernel.Get<SubjectRepository>();
+                studentRepository.Add(GetTestSubjects);
+                studentRepository.Save();
+            }
+        }
+        [Test]
+        public void StudentRepositoryTest_Add_Student()
+        {
+            using (var kernel = new StandardKernel(new ExampleNinjectModule()))
+            {
+                var studentRepository = kernel.Get<StudentRepository>();
+                studentRepository.Add(TestStudentModel);
+                 studentRepository.Save();
+            }
+        }
+
+        private Student TestStudentModel
+        {
+            get
+            {
+                return new Student()
+                {
+                    Name = "TestName",
+                    Age = 26,
+                    AssignedSubjectses = new List<StudentSubject>()
+                    {
+                        new StudentSubject()
+                        {
+                           AssignedSubject = new Subject()
+                           {
+                               Name = "Subject1"
+                           }
+                        },
+                    }
+                };
+            }
+        }
+
+        private IEnumerable<Subject> GetTestSubjects
+        {
+            get
+            {
+                return new List<Subject>
+                {
+                    new Subject
+                    {
+                        Name = "Subject1"
+                    },
+                    new Subject
+                    {
+                        Name = "Subject2"
+                    },
+                    new Subject
+                    {
+                        Name = "Subject4"
+                    },
+                    new Subject
+                    {
+                        Name = "Subject4"
+                    },
+                };
+            }
+        } 
+    }
+}
---
+++ GenericRepositoryUntility/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace GenericRepositoryUntility
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+        void Add(IEnumerable<TEntity> entities);
+        void Edit(TEntity entity);
+        void Save();
+        void Delete(TEntity entity);
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> expression);
+    }
+}
